191da605fcc946a1f8a8843367d71af534f74738
Michal P <michalpawlicki@outlook.com>
2019-04-10 15:11:54 +0200

    Category box minor upgrades

--- index.php
+++ index.php
@@ -2,30 +2,55 @@
 // Always include CSS and JS in the very beginning
 $pageCss = '';
 $pageJs = '';
-require_once ('components/header.php');
+require_once('components/header.php');
 
 ?>
 
-<section class="container">
-    <div class="row">
-        <div class="col-12">
-            <h1>Welcome to <?=$appName?></h1>
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Welcome to <?= $appName ?></h1>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
+        <div class="row d-flex justify-content-center">
             <!-- Squared categories start
                 Should be fetch
              -->
-            <div class="card card-1">
+            <div class="card card-1 p-3">
                 <div class="bottom-text">
                     <h2>Category name</h2>
                 </div>
             </div>
+            <div class="card card-1 p-3">
+                <div class="bottom-text">
+                    <h2>Category name</h2>
+                </div>
+            </div>
+            <div class="card card-1 p-3">
+                <div class="bottom-text">
+                    <h2>Category name</h2>
+                </div>
+            </div>
+            <div class="card card-1 p-3">
+                <div class="bottom-text">
+                    <h2>Category name</h2>
+                </div>
+            </div>
+            <div class="card card-1 p-3">
+                <div class="bottom-text">
+                    <h2>Category name</h2>
+                </div>
+            </div>
+            <div class="card card-1 p-3">
+                <div class="bottom-text">
+                    <h2>Category name</h2>
+                </div>
+            </div>
+
+
         </div>
-    </div>
-</section>
+    </section>
 
 <?php
 
-require_once ('components/footer.php');
+require_once('components/footer.php');
