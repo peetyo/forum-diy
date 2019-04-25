230ec2c11a9ae8f2d850cee91906aeaaaad7afc1
Petar Todorov <petar.bozhidarov.todorov@gmail.com>
2019-04-25 21:59:56 +0200

    updated folder and file names

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once ('Routes.php');
+require_once ('routes.php');
 
 function __autoload($class_name) {
     if (file_exists('./includes/classes/'.$class_name.'.php')){
         require_once './includes/classes/'.$class_name.'.php';
     } else if (file_exists(require_once './includes/Controllers/'.$class_name.'.php')) {
-        require_once './includes/Controllers/'.$class_name.'.php';
+        require_once './includes/controllers/'.$class_name.'.php';
     }
 
 }
