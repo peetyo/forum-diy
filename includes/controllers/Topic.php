b7988674ac6478b4808c1237f3eab69d6f5651b5
Michal P <michalpawlicki@outlook.com>
2019-05-05 12:59:33 +0200

    Pagination works server side

--- includes/controllers/Topic.php
+++ includes/controllers/Topic.php
@@ -2,15 +2,24 @@
 
 class Topic extends Controller
 {
-
-
-    /**
-     *
-     */
     public function GetTopic()
     {
+        //TODO: Catch if the ID is wrong!!!!
+
         // Get the ID of the topic, sanatize it
         $iTopicId = $_GET['id'];
+        // Check which page is it.
+        if (!isset($_GET['page']) || $_GET['page'] == 0 ){
+            /*
+             * OBS: Since we should always start with the page 1, not 0
+             * Server is redirecting to the correct page number. s
+             */
+            $url = $_SERVER['REQUEST_URI'];
+            header("location:$url&page=1");
+        } else {
+            $iPageNumber = $_GET['page'] - 1 ;
+            $iOffset = 5 * $iPageNumber;
+        }
         // Make a call to the database
         $topic = new Model;
         // Make a SELECT statement here, prepare, and execute
@@ -24,8 +33,9 @@ class Topic extends Controller
         if (count($topicContent)) {
             // Michal:  If the topic exists, now we retrieve comments
             $comments = new Model;
-            $sCommentsQuery = $comments->db->prepare('CALL get_comments_for_the_topic(:topicId)');
+            $sCommentsQuery = $comments->db->prepare('CALL get_comments_for_the_topic(:topicId, :offset)');
             $sCommentsQuery->bindValue(':topicId', $iTopicId);
+            $sCommentsQuery->bindValue(':offset', $iOffset);
             $sCommentsQuery->execute();
             $commentsContent = $sCommentsQuery->fetchAll();
             // closing the connection
@@ -43,12 +53,20 @@ class Topic extends Controller
                 $objTopic =new stdClass();
                 $objTopic->topicData = $topicContent;
                 $objTopic->commentData = $commentsContent;
+                $objTopic->paginationData = $iPageNumber;
 
             }
 
 
         }
 
+        /*
+         * Create a veiw, pass object.
+         * OBS: View is created here instead of routes,
+         * otherwise it be impossible/hard to pass additional variables/data
+         * Thanks, Peter, for the solution ;)
+         *
+         */
         self::CreateView('topic', 'topic', '', $objTopic);
 
     }
