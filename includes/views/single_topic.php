06c4377b1ac01271c001ee022b5bcda55e40fca8
Michal P <michalpawlicki@outlook.com>
2019-05-06 23:18:27 +0200

    Front-end kinda ready. Need more styling

--- includes/views/single_topic.php
+++ includes/views/single_topic.php
@@ -4,8 +4,8 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item"><a href="#"><?=$data->topicData["category_name"]?></a></li>
-            <li class="breadcrumb-item active" aria-current="page"><?=$data->topicData["topic_name"]?> </li>
+            <li class="breadcrumb-item"><a href="#"><?= $data->topicData["category_name"] ?></a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?= $data->topicData["topic_name"] ?> </li>
         </ol>
     </nav>
     <div class="row">
@@ -18,8 +18,8 @@
                 <div class="card topic">
                     <div class="card-header bg-dark text-white">
                         <img src="https://via.placeholder.com/25" alt="User's profile picture">
-                        <span class="username"> <?= $data->topicData["username"]?></span> posted on
-                        <span class="comment-date"><?=$data->topicData["date_created"]?></li>
+                        <span class="username"> <?= $data->topicData["username"] ?></span> posted on
+                        <span class="comment-date"><?= $data->topicData["date_created"] ?></li>
                         </span>
                     </div>
                     <div class="card-body">
@@ -43,62 +43,81 @@
                  */
                 // To make it easier, I extract comments from the passed object
                 $comments = $data->commentData;
-                foreach ($comments as $key => $comment){
-                ?>
-                <div class="card comment">
-                    <div class="card-header">
-                        <img src="https://via.placeholder.com/25" alt="User's profile picture">
-                        <span class="username">
-                            <?=$comment["username"]?>
+                foreach ($comments as $key => $comment) {
+                    ?>
+                    <div class="card comment">
+                        <div class="card-header">
+                            <img src="https://via.placeholder.com/25" alt="User's profile picture">
+                            <span class="username">
+                            <?= $comment["username"] ?>
                         </span>
-                        posted on
-                        <span class="comment-date">
-                            <?=$comment["date_created"]?>
+                            posted on
+                            <span class="comment-date">
+                            <?= $comment["date_created"] ?>
                         </span>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                           <?=$comment["content"]?>
-                        </p>
-                    </div>
-                    <div class="text-right card-footer text-muted">
-                        <div class="btn-group" role="group" aria-label="Basic example">
-                            <button type="button" class="btn btn-primary">Reply</button>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                <?= $comment["content"] ?>
+                            </p>
+                        </div>
+                        <div class="text-right card-footer text-muted">
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button type="button" class="btn btn-primary">Reply</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <?php
+                    <?php
                 }
                 ?>
             </div>
             <div class="col-12">
                 <?php
                 // TODO: What the hell is aria-label? Do something about it
+                $currentPage = $data->currentPage;
+                $numberOfPages = $data->numberOfPages;
+                $currentUri = rtrim($data->currentUri, 'page=' . $currentPage);
+                $currentUri = rtrim($currentUri, '&');
                 ?>
                 <nav aria-label="...">
                     <ul class="pagination">
-                        <li class="page-item disabled">
-                            <span class="page-link">Previous</span>
-                        </li>
                         <?php
-                        $currentPage = $data->currentPage;
-                        $numberOfPages = $data->numberOfPages;
-                        $currentUri = $data->currentUri;
-                        for ($page = 1; $page<=$numberOfPages;$page++) {
-                        ?>
-                            <li class="page-item <?=($page == $currentPage) ? "active" : ""?> ">
-                                <a class="page-link" href="<?=$currentUri?>&page=<?=$page?>">
-                                    <?=($page == $currentPage) ? "<span class='sr-only'>" : ""?>
-                                    <?=$page?>
-                                    <?=($page == $currentPage) ? "</span>" : ""?>
+                        /*
+                         * Display Previous only if you're not on the 1st page!
+                         */
+                        if (!($currentPage == 1)) {
+                            ?>
+                            <li class="page-item">
+                                <span class="page-link">
+                                     <a class="page-link" href="<?= $currentUri ?>&page=<?= $currentPage - 1 ?>">
+                                         Previous
+                                     </a>
+                                </span>
+                            </li>
+                            <?php
+                        }
+                        for ($page = 1; $page <= $numberOfPages; $page++) {
+                            ?>
+                            <li class="page-item <?= ($page == $currentPage) ? "active" : "" ?> ">
+                                <a class="page-link" href="<?= $currentUri ?>&page=<?= $page ?>">
+                                    <?= ($page == $currentPage) ? "<span class='sr-only'>" : "" ?>
+                                    <?= $page ?>
+                                    <?= ($page == $currentPage) ? "</span>" : "" ?>
                                 </a>
                             </li>
-                        <?php
+                            <?php
                         }
                         ?>
-                        <li class="page-item">
-                            <a class="page-link" href="#">Next</a>
-                        </li>
+                        <?php
+                        // Display Next only if there's something left
+                        if (!($currentPage == $numberOfPages)) {
+                            ?>
+                            <li class="page-item">
+                                <a class="page-link" href="<?= $currentUri ?>&page=<?= $currentPage + 1 ?>">Next</a>
+                            </li>
+                            <?php
+                        }
+                        ?>
                     </ul>
                 </nav>
             </div>
@@ -109,7 +128,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Topic info</h5>
                     <ul>
-                        <li>Created on <?=$data->topicData["date_created"]?></li>
+                        <li>Created on <?= $data->topicData["date_created"] ?></li>
                         <?php
                         /* Michal: In the <li> below, system checks if there are any comments.
                          * If yes, it echo 'x replies' else 'No replies'
@@ -118,7 +137,8 @@
                         ?>
                     </ul>
                 </div>
-            </div>ro
+            </div>
+            ro
         </div>
     </div>
 </div>
