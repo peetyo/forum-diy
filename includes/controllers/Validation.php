0a454fd563b2b5a60a258a3ef4d958d80be7e8ac
Petar Todorov <petar.bozhidarov.todorov@gmail.com>
2019-05-10 15:28:27 +0200

    merged validation and validations

--- includes/controllers/Validation.php
+++ includes/controllers/Validation.php
@@ -32,6 +32,27 @@ class Validation {
              }
         }
     }
+    
+    // this function cleans the empty spaces  
+    //encoding the html special charecter and sanitize the data
+    public static function sanitize_and_clean($data) {
+        $Newdata = trim($data);   
+        $Newdata = htmlspecialchars($data);
+        $Newdata = filter_var($data,FILTER_SANITIZE_ENCODED);
+        return $Newdata;
+    }
+    //cleans the empty fields and remove html tags
+    public static function sanitize_inputs($data){
+        $Newdata = trim($data);
+        $Newdata = filter_input($data, FILTER_SANITIZE_STRING);
+        return $data;
+    }
+
+    public static function sanitize_output($data){
+        $Newdata = trim($data);
+        $Newdata = filter_input($data, FILTER_SANITIZE_STRING);
+        return $data;
+    }
 }
 // $input = 'a@a.com';
 // Validation::checkInput($input,'email',5,10);
