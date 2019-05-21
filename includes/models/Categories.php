8f5f9972f7323259dcbe956ad71956cbb5f821a8
Martin.G <martin.g.wiredelta@gmail.com>
2019-05-21 16:01:29 +0200

    Refactoring the error logging systems

--- includes/models/Categories.php
+++ includes/models/Categories.php
@@ -10,7 +10,8 @@ class Categories extends Model{
                 return $categories;
             }
         }catch (PDOException $e){
-            $error_log = '{"Eror": '.$e.', "line": '.__LINE__.'}';
+            date_default_timezone_set("Europe/Copenhagen");
+            $error_log = '{"DATE":'.date("Y-m-d").', "TIME": '.date("h:i:sa").' ,"Eror": '.$e.', "line": '.__LINE__.'}';
             file_put_contents('./includes/logs/category.txt', $error_log , FILE_APPEND );
         }
 
