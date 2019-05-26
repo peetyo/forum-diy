66061788aea68a4e4c9dbcf5e511376358e4f1d6
Michal <michalpawlicki@outlook.com>
2019-05-26 19:55:58 +0200

    log saver created, added try catch, #28

--- includes/models/Topics.php
+++ includes/models/Topics.php
@@ -3,136 +3,139 @@
 
 class Topics extends Model
 {
-    public function get_topic($iTopicId, $iOffset){
-        // Make a call to the database
-        // Make a SELECT statement here, prepare, and execute
-        $sTopicContentQuery = $this->db->prepare('CALL get_topic_by_id(:topicId)');
-        $sTopicContentQuery->bindValue(':topicId', $iTopicId);
-        $sTopicContentQuery->execute();
-        $topicContent = $sTopicContentQuery->fetch();
-        // check if anything was received
-        if (count($topicContent)) {
-            // Michal:  If the topic exists, now we retrieve comments
-            // Closing cursor first is to close connection from above
-            $sTopicContentQuery->closeCursor();
-            $sCommentsQuery = $this->db->prepare('CALL get_comments_for_the_topic(:topicId, :offset)');
-            $sCommentsQuery->bindValue(':topicId', $iTopicId);
-            $sCommentsQuery->bindValue(':offset', $iOffset);
-            $sCommentsQuery->execute();
-            $commentsContent = $sCommentsQuery->fetchAll();
-            // closing the connection
-            // $sCommentsQuery = null;
-            // Creating a passing object
-            $objTopic = new stdClass();
-            $objTopic->topicData = $topicContent;
-            $objTopic->commentData = [];
-            $objTopic->whats = $topicContent;
-            $objTopic->numberOfComments = 0;
-            $objTopic->numberOfPages = 1;
-            
-            if (!empty($commentsContent)) {
-                
-                /*
-                * Currently, we have two objects -> $topicContent and $commentsContent
-                * Both are arrays
-                * I suggest merging them into one, so we can pass it as object expected by CreateView
-                */
-                $objTopic->commentData = $commentsContent;
-                
+    public function get_topic($iTopicId, $iOffset)
+    {
+        try {
+            // Make a call to the database
+            // Make a SELECT statement here, prepare, and execute
+            $sTopicContentQuery = $this->db->prepare('CALL get_topic_by_id(:topicId)');
+            $sTopicContentQuery->bindValue(':topicId', $iTopicId);
+            $sTopicContentQuery->execute();
+            $topicContent = $sTopicContentQuery->fetch();
+            // check if anything was received
+            if (count($topicContent)) {
+                // Michal:  If the topic exists, now we retrieve comments
+                // Closing cursor first is to close connection from above
+                $sTopicContentQuery->closeCursor();
+                $sCommentsQuery = $this->db->prepare('CALL get_comments_for_the_topic(:topicId, :offset)');
+                $sCommentsQuery->bindValue(':topicId', $iTopicId);
+                $sCommentsQuery->bindValue(':offset', $iOffset);
+                $sCommentsQuery->execute();
+                $commentsContent = $sCommentsQuery->fetchAll();
+                // closing the connection
+                // $sCommentsQuery = null;
+                // Creating a passing object
+                $objTopic = new stdClass();
+                $objTopic->topicData = $topicContent;
+                $objTopic->commentData = [];
+                $objTopic->whats = $topicContent;
+                $objTopic->numberOfComments = 0;
+                $objTopic->numberOfPages = 1;
 
-                /*
-                 * Calculate number of pages by dividing total
-                 * number of pages by number of results, which is 5
-                 * If you want to change that, please contact
-                 * the database administrator ;)
-                 * AKA change in the routines
-                 *
-                 * So I figured that count($commentsContent) is always returning 5...
-                 * obviously, therefore I need to make another quick call to the database
-                 * to ask how many comments there are
-                 *
-                 * IF YOU HAVE ANOTHER IDEA, YOU'RE WELCOME TO TRY IT OUT
-                 *
-                 */
+                if (!empty($commentsContent)) {
 
-                // Again, closing the connection from abote
-                $sCommentsQuery->closeCursor();
-                $sNumberOfCommentsQuery = $this->db->prepare('CALL get_number_of_comments(:topicId)');
-                $sNumberOfCommentsQuery->bindValue(':topicId', $iTopicId);
-                $sNumberOfCommentsQuery->execute();
-                /*
-                 * That technically should return something since it passed
-                 * check if any comments already.
-                 */
-                $numberOfComments = $sNumberOfCommentsQuery->fetch();
-                $iNumberOfComments = $numberOfComments['totalComments'];
+                    /*
+                    * Currently, we have two objects -> $topicContent and $commentsContent
+                    * Both are arrays
+                    * I suggest merging them into one, so we can pass it as object expected by CreateView
+                    */
+                    $objTopic->commentData = $commentsContent;
 
-                $iNumberOfPages = ceil($iNumberOfComments/5);
-                $objTopic->numberOfComments = $iNumberOfComments;
-                $objTopic->numberOfPages = $iNumberOfPages;
-                // $objTopic->whats = $topicContent;
-                
+
+                    /*
+                     * Calculate number of pages by dividing total
+                     * number of pages by number of results, which is 5
+                     * If you want to change that, please contact
+                     * the database administrator ;)
+                     * AKA change in the routines
+                     *
+                     * So I figured that count($commentsContent) is always returning 5...
+                     * obviously, therefore I need to make another quick call to the database
+                     * to ask how many comments there are
+                     *
+                     * IF YOU HAVE ANOTHER IDEA, YOU'RE WELCOME TO TRY IT OUT
+                     *
+                     */
+
+                    // Again, closing the connection from abote
+                    $sCommentsQuery->closeCursor();
+                    $sNumberOfCommentsQuery = $this->db->prepare('CALL get_number_of_comments(:topicId)');
+                    $sNumberOfCommentsQuery->bindValue(':topicId', $iTopicId);
+                    $sNumberOfCommentsQuery->execute();
+                    /*
+                     * That technically should return something since it passed
+                     * check if any comments already.
+                     */
+                    $numberOfComments = $sNumberOfCommentsQuery->fetch();
+                    $iNumberOfComments = $numberOfComments['totalComments'];
+
+                    $iNumberOfPages = ceil($iNumberOfComments / 5);
+                    $objTopic->numberOfComments = $iNumberOfComments;
+                    $objTopic->numberOfPages = $iNumberOfPages;
+                    // $objTopic->whats = $topicContent;
+
+                }
+
+                return $objTopic;
+
+            } else {
+                return false;
             }
-            
-            return $objTopic;
-
-        }
-        else{
-           return false;
+        } catch (PDOException $error) {
+            LogSaver::save_the_log($error, 'topics.txt');
         }
     }
 
-    public function getTopicsFromCategory($category){
-        try{
+    public function getTopicsFromCategory($category)
+    {
+        try {
             $sQuery = $this->db->prepare('CALL get_topics_from_category(:categoryId)');
             $sQuery->bindValue(':categoryId', $category);
             $sQuery->execute();
             $aTopics = $sQuery->fetchAll();
-            if( count($aTopics) ){
-                
+            if (count($aTopics)) {
+
                 return $aTopics;
                 exit;
-              }
-              return 'Sorry, no topics found in this category';
-        }catch(PDOException $error){
+            }
+            return 'Sorry, no topics found in this category';
+        } catch (PDOException $error) {
             // Correct this error for production
             echo '{"status": 0, "message": "Sorry, something went wrong. Try again later."}';
-            $error_log = '{"Eror": '.$error.', "line": '.__LINE__.'}';
-            file_put_contents('./includes/logs/topics.txt', $error_log , FILE_APPEND );
-            // return $error; Add the error log!
+            LogSaver::save_the_log($error, 'topic.txt');
         }
 
     }
 
-    public function create_topic($topicData){
+    public function create_topic($topicData)
+    {
         // print_r($topicData);    
-        try{
-                $db = $this->db;
-                $sQuery = $db->prepare('INSERT INTO `topics` VALUES (NULL,:topic_name,NULL,:category_id,:user,:content)');
-                // Could not get the last inserted ID when using the stored procedure
-                // The procedure code itself has to be updated somehow, but I didnt manage 
-                // to make it work. - PETER
-                // $sQuery = $db->prepare('CALL create_topic(:topic_name, :category_id, :user_id, :content)');
-                $sQuery->bindValue(':topic_name', $topicData['topic_name']);
-                $sQuery->bindValue(':category_id', $topicData['category_id']);
-                $sQuery->bindValue(':user', $topicData['user_id']);
-                $sQuery->bindValue(':content', $topicData['content']);
-                $sQuery->execute();
-                if(!$sQuery->rowCount()){
-                  echo '{"status": 0, "message": "Sorry, something went wrong when creating topic."}';
-                  exit();
-                }
-                $id = $db->lastInsertId();
-                // Remember to update this echo once its paired with some AJAX
-                echo '{"status": 1, "message": "topic created", "topic": '.$id.' }';
-            }catch(PDOException $error){
-
-                echo '{"status": 0, "message": "Sorry, something went wrong. Try again later."}';
-            date_default_timezone_set("Europe/Copenhagen");
-            $error_log = '{"DATE":'.date("Y-m-d").', "TIME": '.date("h:i:sa").' , "Eror": '.$error.', "line": '.__LINE__.'}';
-            file_put_contents('./includes/logs/topics.txt', $error_log , FILE_APPEND );
+        try {
+            $db = $this->db;
+            $sQuery = $db->prepare('INSERT INTO `topics` VALUES (NULL,:topic_name,NULL,:category_id,:user,:content)');
+            // Could not get the last inserted ID when using the stored procedure
+            // The procedure code itself has to be updated somehow, but I didnt manage
+            // to make it work. - PETER
+            // $sQuery = $db->prepare('CALL create_topic(:topic_name, :category_id, :user_id, :content)');
+            $sQuery->bindValue(':topic_name', $topicData['topic_name']);
+            $sQuery->bindValue(':category_id', $topicData['category_id']);
+            $sQuery->bindValue(':user', $topicData['user_id']);
+            $sQuery->bindValue(':content', $topicData['content']);
+            $sQuery->execute();
+            if (!$sQuery->rowCount()) {
+                echo '{"status": 0, "message": "Sorry, something went wrong when creating topic."}';
                 exit();
             }
+            $id = $db->lastInsertId();
+            // Remember to update this echo once its paired with some AJAX
+            echo '{"status": 1, "message": "topic created", "topic": ' . $id . ' }';
+        } catch (PDOException $error) {
+
+            echo '{"status": 0, "message": "Sorry, something went wrong. Try again later."}';
+            date_default_timezone_set("Europe/Copenhagen");
+            LogSaver::save_the_log($error, 'topics.txt');
+            exit();
+        }
     }
 
 }
--- includes/models/Users.php
+++ includes/models/Users.php
@@ -1,71 +1,94 @@
 <?php
 // for testing this class we need to include the Model.php
 // require_once 'Model.php';
-class Users extends Model {
-    public function read_users() {
-        $sQuery = $this->db->prepare('SELECT * FROM users');
-        $sQuery->execute();
-        $users = $sQuery->fetchAll();
-        if( count($users) ){
-            echo print_r($users);
-            exit;
+class Users extends Model
+{
+    public function read_users()
+    {
+        try {
+            $sQuery = $this->db->prepare('SELECT * FROM users');
+            $sQuery->execute();
+            $users = $sQuery->fetchAll();
+            if (count($users)) {
+                echo print_r($users);
+                exit;
+            }
+        } catch (PDOException $error) {
+            LogSaver::save_the_log($error, 'users.txt');
         }
     }
-    //prepared insert stament, whcih is used in sign_up controller
-    public function sign_up_user($username, $hashed_pass, $email ){
 
-        $checkUserQuery = $this->db->prepare( 'SELECT username, email FROM users WHERE email = :email OR username = :username');
-        $checkUserQuery->bindValue( ':email', $email);
-        $checkUserQuery->bindValue( ':username', $username);
-        $checkUserQuery->execute();
-        // check if the user exists in the dabase
-        if(!empty($checkUserQuery->fetch())){   
-            // return false to the controllers
-            // return false;
-            //PETER: the response here is sent to AJAX and AJAX expects a JSON
-            // These 2 have to be separated to show something meaningful to the user
-            // we gotta tell them if the email is already in use of if the username is already in use
-            // specifically.
-            echo '{"status":"0","message":"Duplicating username or email"}';
-            exit;  
+    //prepared insert stament, which is used in sign_up controller
+    public function sign_up_user($username, $hashed_pass, $email)
+    {
+        try {
+            $checkUserQuery = $this->db->prepare('SELECT username, email FROM users WHERE email = :email OR username = :username');
+            $checkUserQuery->bindValue(':email', $email);
+            $checkUserQuery->bindValue(':username', $username);
+            $checkUserQuery->execute();
+            // check if the user exists in the dabase
+            if (!empty($checkUserQuery->fetch())) {
+                // return false to the controllers
+                // return false;
+                //PETER: the response here is sent to AJAX and AJAX expects a JSON
+                // These 2 have to be separated to show something meaningful to the user
+                // we gotta tell them if the email is already in use of if the username is already in use
+                // specifically.
+                echo '{"status":"0","message":"Duplicating username or email"}';
+                exit;
+            }
+        } catch (PDOException $error) {
+            LogSaver::save_the_log($error, 'users.txt');
         }
 
-
-
-        $sQuery = $this->db->prepare('INSERT INTO users
+        try {
+            $sQuery = $this->db->prepare('INSERT INTO users
          VALUES(null, :userName, :hashed_password, :email, :date_created, :user_role, :active )');
-        $sQuery->bindValue(':userName',$username );
-        $sQuery->bindValue(':hashed_password',$hashed_pass );
-        $sQuery->bindValue(':email',$email );
-        $sQuery->bindValue(':date_created',date('Y/m/d H:i:s') );
-        $sQuery->bindValue(':user_role', 4 );
-        $sQuery->bindValue(':active', 0 );
-        $sQuery->execute();
-        if( $sQuery->rowCount() ){
-           echo '{"status":"1", "message":"User created"}';
-           exit;
+            $sQuery->bindValue(':userName', $username);
+            $sQuery->bindValue(':hashed_password', $hashed_pass);
+            $sQuery->bindValue(':email', $email);
+            $sQuery->bindValue(':date_created', date('Y/m/d H:i:s'));
+            $sQuery->bindValue(':user_role', 4);
+            $sQuery->bindValue(':active', 0);
+            $sQuery->execute();
+            if ($sQuery->rowCount()) {
+                echo '{"status":"1", "message":"User created"}';
+                exit;
+            }
+            echo '{"status":"0","message":"User was not created"}';
+        } catch (PDOException $error) {
+            LogSaver::save_the_log($error, 'users.txt');
         }
-        echo '{"status":"0","message":"User was not created"}';
     }
 
     // :MORTIMUS the results of function  will be used to check if the username exist or not
     // the function takes the username from table
-    public function select_username($username){
-        $sQuery = $this->db->prepare('SELECT username from users WHERE username = :usersname');
-        $sQuery->bindValue(':usersname', $username);
-        $sQuery->execute();
-        $aUser = $sQuery->fetchAll();
-       
-        return $aUser;
+    public function select_username($username)
+    {
+        try {
+            $sQuery = $this->db->prepare('SELECT username from users WHERE username = :usersname');
+            $sQuery->bindValue(':usersname', $username);
+            $sQuery->execute();
+            $aUser = $sQuery->fetchAll();
+
+            return $aUser;
+        } catch (PDOException $error) {
+            LogSaver::save_the_log($error, 'users.txt');
+        }
     }
 
-    public function select_username_and_password($username){
-        $sQuery = $this->db->prepare('SELECT id,username,password_hashed,email  from users WHERE username = :usersname');
-        $sQuery->bindValue(':usersname', $username);
-        $sQuery->execute();
-        $aUser = $sQuery->fetchAll();
-       
-        return $aUser;
+    public function select_username_and_password($username)
+    {
+        try {
+            $sQuery = $this->db->prepare('SELECT id,username,password_hashed,email  from users WHERE username = :usersname');
+            $sQuery->bindValue(':usersname', $username);
+            $sQuery->execute();
+            $aUser = $sQuery->fetchAll();
+
+            return $aUser;
+        } catch (PDOException $error) {
+            LogSaver::save_the_log($error, 'users.txt');
+        }
     }
 }
 // for testing
